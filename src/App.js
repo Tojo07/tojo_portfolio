e7631e077944e3e84b2cdb96000a0363c0722da2
Tojo07 <tsimalayh@berea.edu>
2024-12-23 22:01:07 -0500

    almost finished with mobile

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   }, []);
 
   return (
-    <section class='h-screen min-h-screen lg:h-screen lg:w-screen bg-[#bab6aa] lg:px-12 lg:py-4 font-main lg:flex lg:flex-col sm:overflow-auto'>
-      <div class='h-[10%] w-full bg-[#5d5c5a] text-[#151724] font-normal lg:rounded-md mb-4 flex justify-between lg:animate-show'>
+    <section class='h-screen lg:h-screen lg:w-screen bg-[#bab6aa] lg:px-12 lg:py-4 font-main lg:flex lg:flex-col'>
+      <div class='h-[10%] w-full bg-[#5d5c5a] text-[#151724] font-normal lg:rounded-md lg:mb-4 flex justify-between lg:animate-show'>
         <div class='h-full w-[20%] flex justify-start place-items-center pl-4 cursor-default'>
           <h1 class={`transition-opacity duration-1000 delay-[${text_display_delay}] ${loaded ? "opacity-100" : "opacity-0"
             } ease-in-out font-normal text-lg`}>Tojo <span class='font-semibold'>Tsimalay</span></h1>
@@ -24,21 +24,21 @@ function App() {
           <button onClick={() => window.open('https://medium.com/@tsimalayheriniainatojo', '_blank')} class='hover:text-[#2E3042] transition delay-200 ease-in-out'>Blog</button>
         </div>
       </div>
-      <div class='min-h-full h-full lg:h-[88%] w-full lg:flex'>
-        <div class='h-[68%] w-full lg:h-full lg:w-[68%] lg:mr-4 overflow-scroll'>
-          <div class='h-[70%] w-full lg:flex lg:mb-4'>
-            <div class='relative h-[64%] w-full lg:h-full lg:w-[64%] bg-[#5d5c5a] lg:rounded-md lg:mr-4 animate-show p-4 flex text-center lg:text-start justify-center place-items-center text-[#151724]'
+      <div class='h-[90%] min-h-[100vh] lg:h-[88%] w-full lg:flex sm:relativ overflow-scroll lg:overflow-hidden'>
+        <div class='h-[70%] w-full lg:h-full lg:w-[68%] lg:mr-4'>
+          <div class='h-[60%] lg:h-[69%] w-full lg:flex lg:mb-4'>
+            <div class='relative h-[45%] w-full lg:h-full lg:w-[64%] bg-[#5d5c5a] lg:rounded-md lg:mr-4 animate-show p-4 flex text-center lg:text-start justify-center place-items-center text-[#151724]'
               style={{ '--start-left': '500px' }}>
               <p class={`text-3xl lg:text-5xl font-black leading-tight transition-opacity duration-1000 delay-[${text_display_delay}] ${loaded ? "opacity-100" : "opacity-0"
                 } ease-in-out`}>Student Software Engineer and <span class='font-light text-[#DAD3C8] bg-[#151724] p-1 text-2xl lg:text-4xl'>Photography</span> Enthousiast</p>
             </div>
-            <div class='relative h-[66%] w-full lg:h-full lg:w-[34%] top-1/2 left-1/2 lg:rounded-md bg-[url("../public/IMG_4259.JPG")] bg-cover bg-top bg-clip-border animate-slide z-50'>
+            <div class='relative h-[60%] w-full lg:h-full lg:w-[34%] top-1/2 left-1/2 lg:rounded-md bg-[url("../public/IMG_4259.JPG")] bg-cover bg-top bg-clip-border animate-slide z-50'>
             </div>
           </div>
-          <div class='h-[50%] w-full lg:h-[30%] lg:w-full lg:flex lg:pb-2 pb-16'>
-            <div class='relative h-full w-full bg-[#5d5c5a] lg:rounded-md animate-show lg:mr-4'
+          <div class='h-[40%] w-full lg:h-[30%] lg:w-full lg:flex lg:pb-2'>
+            <div class='relative h-[50%] lg:h-full w-full bg-[#5d5c5a] lg:rounded-md animate-show lg:mr-4'
               style={{ '--start-top': '-200px', '--start-left': '500px' }}></div>
-            <button onClick={() => { window.open(resume, '_blank') }} class='relative h-full w-full bg-[#151724] lg:rounded-md animate-show transition duration-200 hover:bg-[#2E3042]'
+            <button onClick={() => { window.open(resume, '_blank') }} class='relative h-[50%] lg:h-full w-full bg-[#151724] lg:rounded-md animate-show transition duration-200 hover:bg-[#2E3042]'
               style={{ '--start-top': '-200px', '--start-left': '100px' }}>
               <div class={`h-[14%] w-full flex justify-end place-items-top p-4 transition-opacity duration-1000 delay-[${text_display_delay}] ${loaded ? "opacity-100" : "opacity-0"
                 } ease-in-out`}>
@@ -52,8 +52,8 @@ function App() {
           </div>
         </div>
 
-        {/* <div class='h-[32%] w-full lg:h-full lg:w-[32%]'>
-          <div class='relative h-[79%] w-full bg-[#5d5c5a] rounded-md mb-4 animate-show lg:px-4'
+        <div class='h-[80%] w-full lg:h-full lg:w-[32%]'>
+          <div class='relative h-[86%] w-full bg-[#5d5c5a] lg:rounded-md lg:mb-4 animate-show lg:px-4'
             style={{ '--start-left': '-300px' }}>
             <div class={`h-[10%] w-full flex justify-between place-items-top pt-4 transition-opacity duration-1000 delay-[${text_display_delay}] ${loaded ? "opacity-100" : "opacity-0"
               } ease-in-out`}>
@@ -69,7 +69,7 @@ function App() {
               <div class='w-full h-[34%] border-t-2 border-[#151724] flex justify-start place-items-center'>Lifestyle</div>
             </div>
           </div>
-          <div class='relative h-[19%] w-full bg-[#5d5c5a] rounded-md animate-show flex justify-center place-items-center px-4'
+          <div class='relative h-[14%] w-full bg-[#5d5c5a] lg:rounded-md animate-show flex justify-center place-items-center px-4'
             style={{ '--start-top': '-200px', '--start-left': '-300px' }}>
             <div class={`h-full w-[60%] flex justify-between place-items-center transition-opacity duration-1000 delay-[${text_display_delay}] ${loaded ? "opacity-100" : "opacity-0"
               } ease-in-out`}>
@@ -78,7 +78,7 @@ function App() {
               <button onClick={() => window.open('https://www.instagram.com/tojo.mg/', '_blank')}><svg class='size-8 hover:fill-[#2E3042] fill-[#151724] ease-in-out duration-200' viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z"></path> <path d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z"></path> <path fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z"></path> </g></svg></button>
             </div>
           </div>
-        </div> */}
+        </div>
       </div>
     </section>
   );
